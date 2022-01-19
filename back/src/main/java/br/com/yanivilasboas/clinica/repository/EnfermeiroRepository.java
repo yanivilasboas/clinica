0862fc123a0ed2ac4307da81dd2b2eae5d8a8951
yanivilasboas <yanivilasboas@gmail.com>
2022-01-19 14:45:42 -0300

    Removendo back

--- back/src/main/java/br/com/yanivilasboas/clinica/repository/EnfermeiroRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.yanivilasboas.clinica.repository;
-
-import br.com.yanivilasboas.clinica.domain.Enfermeiro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnfermeiroRepository  extends JpaRepository<Enfermeiro, Long> {
-}
--- back/src/main/java/br/com/yanivilasboas/clinica/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.yanivilasboas.clinica.repository;
-
-import br.com.yanivilasboas.clinica.domain.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PacienteRepository extends JpaRepository<Paciente, String> {
-}
